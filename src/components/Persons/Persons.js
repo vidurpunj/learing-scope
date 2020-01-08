54f6812f3a7c6c8fa43eec1ef53a216e4c54e23e
vidur.tukatech <vidurp@tukatech.com>
2020-01-08 18:24:48 +0530

    master update

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => persons.map((person, index) => {
+const persons = (props) => props.persons.map((person, index) => {
     return <Person
         click = {() => props.clicked(index)}
         name={person.name}
